fa43329aafdcecfabc852e68cf6e735c25e82b65
Britney Smith <brit.smith7@gmail.com>
2018-05-20 19:13:35 -0400

    Completed Exercise 4 for Day 1

---
+++ Day1/Exercise 4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> aliveEmployeeList = new List<string>();  
+                aliveEmployeeList.Add("Jane");
+                aliveEmployeeList.Add("Mark");
+                aliveEmployeeList.Add("Scott");
+                aliveEmployeeList.Add("Lisa");
+                aliveEmployeeList.Add("Kim");
+                aliveEmployeeList.Add("Mike");
+                aliveEmployeeList.Add("Jack");
+                aliveEmployeeList.Add("Sue");
+                aliveEmployeeList.Add("Chris");
+                aliveEmployeeList.Add("Michele");
+            
+
+            foreach (string employee in aliveEmployeeList) {
+                    Console.WriteLine(employee +": Alive");
+            }
+
+
+        }
+    }
+}
