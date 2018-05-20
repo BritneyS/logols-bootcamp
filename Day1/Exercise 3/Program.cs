b84783b10d7acbc4d51aa042928b590125450382
Britney Smith <brit.smith7@gmail.com>
2018-05-20 13:12:32 -0400

    Completed Exercise 3 for Day1

---
+++ Day1/Exercise 3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        public static string checkStatus (int status, string name) {
+            string status_name = " ";
+            string name_text = name;
+            switch (status)
+            {
+                case 1: 
+                    status_name = "Alive";
+                    break;
+                case 2:
+                    status_name = "Zombie";
+                    break;
+                case 3: 
+                    status_name = "Dead";
+                    break;
+                case 4: 
+                    status_name = "Unknown";
+                    break;
+                default: 
+                    status_name = "None";
+                    break;
+            }
+
+            return name_text +": "+ status_name;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] status = new int[] {1, 2, 3, 4};
+            string[] names = new string[] {"Jane", "John", "Sue", "Mike"};
+            
+            for (int i = 0; i < status.Length; i++) {
+                Console.WriteLine(checkStatus(status[i], names[i]));
+            }
+            //-----
+            void writeName (string name) {
+                Console.WriteLine(name);
+            }
+        }
+
+        
+    }
+}
