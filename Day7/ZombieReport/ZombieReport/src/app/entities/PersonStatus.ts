2305a7d75b5d538b6cd519383f7eff62adb35c70
Britney Smith <brit.smith7@gmail.com>
2018-06-30 13:45:20 -0400

    Completed main html component

---
+++ Day7/ZombieReport/ZombieReport/src/app/entities/PersonStatus.ts
@@ -0,0 +1,12 @@
+export class PersonStatus {
+    //public personID:number;
+
+    public firstName:string;
+
+    public lastName:string;
+
+    public personStatusId:number;
+
+    public statusDescription:string;
+
+}
--- Day7/ZombieReport/ZombieReport/src/app/zombieReport.component.ts
+++ Day7/ZombieReport/ZombieReport/src/app/zombieReport.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PersonStatus } from './entities/PersonStatus';
 
 @Component({
   selector: 'zombie-report',
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./zombieReport.component.css']
 })
 export class ZombieReportComponent {
+  public title:string = "Zombie Status Report";
 
+  public statuses:PersonStatus[] = [];
+
+  public constructor() {
+    this.statuses = [
+      {
+        //personID: 1,
+        firstName: 'Tom',
+        lastName: 'Brady',
+        personStatusId: 4,
+        statusDescription: 'Unknown'
+      },
+
+      {
+        //personID: 2,
+        firstName: 'Lisa',
+        lastName: 'Vane',
+        personStatusId: 2,
+        statusDescription: 'Zombie'
+      },
+
+      {
+        //personID: 3,
+        firstName: 'John',
+        lastName: 'Jones',
+        personStatusId: 3,
+        statusDescription: 'Dead'
+      },
+
+      {
+        //personID: 5,
+        firstName: 'Valerie',
+        lastName: 'Thompson',
+        personStatusId: 1,
+        statusDescription: 'Alive'
+      },
+
+      {
+        //personID: 6,
+        firstName: 'Michael',
+        lastName: 'Masters',
+        personStatusId: 1,
+        statusDescription: 'Alive'
+      },
+
+    ]
+  }
 }
