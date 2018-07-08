0cb445c286200baded9027179e522d3490e3363e
Britney Smith <brit.smith7@gmail.com>
2018-07-08 12:33:52 -0400

    Added table joins

---
+++ FinalProject/sql/join-tables.sql
@@ -0,0 +1,11 @@
+use gym;
+
+SELECT  m.MemberID, m.FirstName, m.LastName,
+        ms.MemberStatusDescription,
+        c.ClassDescription
+FROM    member m
+        INNER JOIN memberstatus ms
+            ON m.MemberStatusID = ms.MemberStatusID
+        LEFT OUTER JOIN Class c
+            ON m.ClassID = c.ClassID
+ORDER BY MemberID ASC;
