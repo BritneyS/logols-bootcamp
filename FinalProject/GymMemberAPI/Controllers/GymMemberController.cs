8bc384516b4133a8a7844c61dab5726bc9b084cd
Britney Smith <brit.smith7@gmail.com>
2018-07-10 17:00:44 -0400

    Added controller, tested JSON payload

---
+++ FinalProject/GymMemberAPI/Controllers/GymMemberController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymMemberAPI.Controllers
+{
+    [Route("api/[controller]")] //URL: .../api/[controller] where [controller] is controller name
+    public class GymMemberController : Controller
+    {
+        GymMemberRepository repository;
+
+        public GymMemberController()
+        {
+            repository = new GymMemberRepository();
+        }
+
+        // GET api/GymMember
+        [HttpGet]
+        public List<GymMemberAPI.Member> Get()
+        {
+            return repository.GetAll();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]GymMemberAPI.Member Member)
+        {
+           // repository.Insert(Member);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ FinalProject/GymMemberAPI/GymMemberRepository.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+
+
+
+using System.Data;
+using System.Linq;
+using Dapper;
+
+namespace GymMemberAPI
+{
+    public class GymMemberRepository : Repository
+    {
+        public List<Member> GetAll()
+        {
+            using (IDbConnection dbConnection = Connection) //Connection: property that exists in base class Repository
+            {
+                dbConnection.Open();
+
+                string sql = "SELECT m.MemberID, m.FirstName, m.LastName, ms.MemberStatusDescription, c.ClassDescription "
+                + "FROM member m "
+                + "INNER JOIN memberstatus ms "
+                + "ON m.MemberStatusID = ms.MemberStatusID "
+                + "LEFT OUTER JOIN Class c "
+                + "ON m.ClassID = c.ClassID "
+                + "ORDER BY MemberID ASC;";
+
+                return dbConnection.Query<Member>(sql, commandType: CommandType.Text).ToList();
+
+            }
+        }
+
+        /* public void Insert(Person Person)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                dbConnection.Execute(
+                    "INSERT into zombiereport.person (FirstName, LastName, PersonStatusID) "
+                    + "VALUES (@FirstName, @LastName, @PersonStatusID);",
+                     new {
+
+                        FirstName = Person.FirstName,
+                        LastName = Person.LastName,
+                        PersonStatusID = Person.PersonStatusID
+                    },
+                    commandType: CommandType.Text
+                );
+
+            }
+        } */
+    }
+ 
+}
