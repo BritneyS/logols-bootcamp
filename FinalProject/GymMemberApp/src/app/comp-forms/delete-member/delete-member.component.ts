6a312a30167e7070d3128a341229f31564ab5a4d
Britney Smith <brit.smith7@gmail.com>
2018-07-22 07:20:07 -0400

    Delete member component changes

--- FinalProject/GymMemberApp/src/app/comp-forms/delete-member/delete-member.component.ts
+++ FinalProject/GymMemberApp/src/app/comp-forms/delete-member/delete-member.component.ts
@@ -11,18 +11,42 @@ import { MemberStatusService } from '../../services/MemberStatusService';
 export class DeleteMemberComponent {
   //TODO: change for deleting member
   public memberStatus:MemberStatus;
+  public statuses:MemberStatus[] = [];
+  public filter:string = '';
+  public filteredstatuses:MemberStatus[] = [];
 
   public constructor(private memberStatusService: MemberStatusService) {
     this.memberStatus = new MemberStatus();
+    memberStatusService.getAll().subscribe(result => { 
+      for (let status of result) {
+        this.statuses.push(status);
+        this.filteredstatuses.push(status);
+      }
+      this.statuses = this.filteredstatuses;
+    }, error => {
+      console.log(error)
+    });
   }
 
   public getIDClick():void {
     //console.log('test');
     console.log(this.memberStatus);
-    this.memberStatusService.get(this.memberStatus.memberID).subscribe(() => {
+    this.memberStatusService.get(this.memberStatus.memberID).subscribe(result => {
+      for (let status of result) {
+        if (status.memberID == this.memberStatus.memberID) {
+          console.log("status.memberID: " + status.memberID);
+          this.filteredstatuses.push(status);
+        }
+        else {
+          console.log("condition false");
+        }
+      }
+      this.statuses = this.filteredstatuses;
       console.log("person retrieved");
-      console.log(this.memberStatus);
-    }, error => console.error(error));
+      console.log(this.filteredstatuses);
+    }, error => {
+      console.log(error)
+    })
   }
 
 
